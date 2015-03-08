43e318d6705ae24364c0dd8bf9c31e8d6cff595d
Mors <morsangelos@hotmail.com>
2015-03-07 21:28:23 -0800

    Added a bit of anchoring code so that we can sync up movement independent of transform hierarchy

---
+++ Assets/Canal/Anchoring/Anchor.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Anchor : MonoBehaviour {
+    [System.Serializable]
+    public struct TransformInfo
+    {
+        public Vector3 position;
+        public Vector3 scale;
+        public Quaternion rotation;
+    }
+
+
+    public Transform root;
+
+    public delegate void TransformUpdatedHandler(TransformInfo relativeTransform);
+    public event TransformUpdatedHandler TransformUpdated;
+
+    private Transform cachedTransform;
+
+    private TransformInfo currentTransform;
+    public TransformInfo CurrentTransform
+    {
+        get { return this.currentTransform; }
+    }
+
+    public void OnEnable()
+    {
+        UpdateTransform(true);
+    }
+
+    public void FixedUpdate()
+    {
+        UpdateTransform();
+    }
+
+    public void UpdateTransform(bool force = false)
+    {
+        cachedTransform = cachedTransform ?? transform;
+
+        TransformInfo transformInfo = new TransformInfo();
+        if(root == null || root.hasChanged || force)
+        {
+            if(cachedTransform.hasChanged || force)
+            {
+                Transform currentParent = cachedTransform.parent;
+                cachedTransform.parent = root;
+
+                transformInfo.position = cachedTransform.localPosition;
+                transformInfo.scale = cachedTransform.localScale;
+                transformInfo.rotation = cachedTransform.localRotation;
+                
+                cachedTransform.parent = currentParent;
+
+                currentTransform = transformInfo;
+                if(TransformUpdated != null)
+                {
+                    TransformUpdated(transformInfo);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Canal/Anchoring/AnchorTarget.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnchorTarget : MonoBehaviour {
+    public Anchor anchor;
+    public Transform root;
+
+    private Transform cachedTransform;
+
+    public void OnEnable()
+    {
+        this.UpdatePosition(anchor.CurrentTransform);
+        anchor.TransformUpdated += UpdatePosition;
+    }
+
+    public void OnDisable()
+    {
+        anchor.TransformUpdated -= UpdatePosition;
+    }
+
+    public void UpdatePosition(Anchor.TransformInfo relativeTransform)
+    {
+        cachedTransform = cachedTransform ?? transform;
+
+        Transform currentParent = cachedTransform.parent;
+        
+        cachedTransform.localPosition = relativeTransform.position;
+        cachedTransform.localScale = relativeTransform.scale;
+        cachedTransform.localRotation = relativeTransform.rotation;
+        
+        cachedTransform.parent = currentParent;
+    }
+}
